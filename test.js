ed2923627fabb650ca1720fc14a1bc283ebad740
kwaman <kwaman@gmail.com>
2015-05-14 17:32:54 -0700

    parse city,country

--- test.js
+++ test.js
@@ -7,6 +7,11 @@ $( document ).ready(function() {
     geocoder.geocode({'address': address}, function(results, status) {
       if (status == google.maps.GeocoderStatus.OK) {
           var location = results[0].geometry.location;
+          //var city = results[0].address_components[0].long_name;
+          var city = parseLocation(results,'locality');
+          var country = parseLocation(results,'country');
+          console.log(city);
+          console.log(country);
           getTime(location.lat(),location.lng());
       } else {
         alert('Geocode was not successful for the following reason: ' + status);
@@ -23,11 +28,22 @@ $( document ).ready(function() {
             'username' : 'timezone339df08d'
           })
       }).then(function(data) {
-         $('.time').append(data.time);
-         $('.countryName').append(data.countryName);
-         $('.lat').append(data.lat);
+         $('.time').empty().append(data.time);
+         $('.countryName').empty().append(data.countryName);
+         $('.lat').empty().append(data.lat);
       });
   };
+
+  function parseLocation(results, type){
+    for(var addComponent in results[0].address_components){
+      var component = results[0].address_components[addComponent];
+      for(typeIndex in component.types ){
+        if(component.types[typeIndex]==type) {
+          return component.long_name;
+        }
+      }
+    }
+  }
 
   // Usage:
   //   var data = { 'first name': 'George', 'last name': 'Jetson', 'age': 110 };
