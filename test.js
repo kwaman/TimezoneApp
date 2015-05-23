a0def5cfba09ca2f9024fc00ec860f16f7f0cc59
kwaman <kwaman@gmail.com>
2015-05-22 21:08:48 -0700

    Changes

--- test.js
+++ test.js
@@ -1,18 +1,21 @@
 $( document ).ready(function() {
 
-  function codeAddress() {
+  function getLocation() {
     var geocoder = new google.maps.Geocoder();
     var address = document.getElementById('address').value;
 
+    // Get the location submitted
     geocoder.geocode({'address': address}, function(results, status) {
       if (status == google.maps.GeocoderStatus.OK) {
           var location = results[0].geometry.location;
-          //var city = results[0].address_components[0].long_name;
+
+          // Parse out the city and country
           var city = parseLocation(results,'locality');
           var country = parseLocation(results,'country');
-          console.log(city);
-          console.log(country);
+
+          // Get local time for that Lat,Lng
           getTime(location.lat(),location.lng());
+
       } else {
         alert('Geocode was not successful for the following reason: ' + status);
       }
@@ -21,6 +24,8 @@ $( document ).ready(function() {
 
   function getTime(lat, lng) {
       var baseurl = "http://api.geonames.org/timezoneJSON";
+      var time;
+
       $.ajax({
           url: baseurl + '?' + encodeQueryData({
             'lat' : lat,
@@ -28,12 +33,39 @@ $( document ).ready(function() {
             'username' : 'timezone339df08d'
           })
       }).then(function(data) {
-         $('.time').empty().append(data.time);
-         $('.countryName').empty().append(data.countryName);
-         $('.lat').empty().append(data.lat);
+        addClock(data.time);
       });
   };
 
+  function addClock(time){
+    if ($('div.loop').length<10) {
+      var clonedDiv=$($('div.loop')[0]).clone().appendTo('.parentDiv');
+      clonedDiv.html(time);
+      clonedDiv.css({"font-size":"200%", "font-color":"white", "font-family":"Helvetica"});
+      switch($('div.loop').length) {
+        case 1:
+          clonedDiv.css("background-color","green");
+          break;
+        case 2:
+          clonedDiv.css("background-color","blue");
+          break;
+        case 3:
+          clonedDiv.css("background-color","yellow");
+          break;;
+        default:
+          clonedDiv.css("background-color","orange");
+          break;
+      }
+    }
+  };
+
+  function changeBackgroundColor(element,length)
+  {
+
+  }
+
+  // Helper
+  // Parse out the location from the Google API result
   function parseLocation(results, type){
     for(var addComponent in results[0].address_components){
       var component = results[0].address_components[addComponent];
@@ -56,6 +88,6 @@ $( document ).ready(function() {
   };
 
   document.getElementById("ajaxButton").onclick = function() {
-    codeAddress();
+    getLocation();
   };
 });
